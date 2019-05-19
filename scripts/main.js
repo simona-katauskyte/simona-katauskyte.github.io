8a3df256406d187bb8eaa86945262c2f490428eb
Simona Katauskyte <simona.katauskyte@gmail.com>
2019-05-19 15:46:03 +0300

    added new card, created second btn for cards, deleted excessive js code

--- scripts/main.js
+++ scripts/main.js
@@ -10,35 +10,4 @@ function filterSelection(technologyName) {
   for (var j = 0; j < y.length; j++) {
     y[j].style.display = "flex";
   }
-
-  // let technology = document.getElementById("technology");
-  // technology.innerHTML = technologyName.toUpperCase();
 }
-
-
-
-// function openProject(projectName) {
-//   var i;
-//   var x = document.getElementsByClassName("projects");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";  
-//   }
-//   document.getElementById(projectName).style.display = "block";  
-// }
-
-
-
-// function sortSelection() {
-// 	let rawArray = document.getElementsByClassName("date");
-
-// 	let dateArray = [];
-
-// 	for (var i = 0; i < rawArray.length; i++) {
-// 		let date = rawArray[i].innerHTML;
-// 		dateArray.push(date);
-// 	}
-
-// 	let dateArraySorted = dateArray.sort((a, b) => b - a);
-
-// 	console.log(dateArraySorted);
-// }
